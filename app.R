6cdab442aff219bb568acbf61c0f8ee4069d1a05
leonhardt913 <673409776@qq.com>
2023-11-20 15:14:02 +0800

    change name 231120

--- app.R
+++ app.R
@@ -4,69 +4,69 @@ library(shinycssloaders)
 
 
 ui <- fluidPage(
-  navbarPage(title = "PPI-tools",
-             
-             #==============================Main page================================#  
+  navbarPage(title = "PPI tool",
+
+             #==============================Main page================================#
              tabPanel("Home",
-                      
-                      titlePanel(h1("Welcome ",align="center",style="color: skyblue")), 
-                      
-             ), 
-             
-             
-             #==============================Analysis page================================#    
+
+                      titlePanel(h1("Welcome ",align="center",style="color: skyblue")),
+
+             ),
+
+
+             #==============================Analysis page================================#
              tabPanel("Analysis",
-                      
+
                       fluidRow(
-                        
-                        
+
+
                         sidebarLayout(
                           sidebarPanel(
-                            
-                            
+
+
                             #输入主要data
                             fileInput("file1", "Choose CSV file", accept = ".csv"),
-                            
+
                           ),
-                          
+
                           mainPanel(
-                            
+
                             tabsetPanel(
-                              
+
                               #loading effect withspinner()
                               tabPanel("List",withSpinner(tableOutput("contents"))),
-                              
+
                             )
-                          ), 
-                          
+                          ),
+
                         )
-                        
+
                       )
              ),
-             
+
              #==============================Documentation================================#
-             
+
              tabPanel("Documentation",
                       mainPanel(
-                        
+
                         h2("Preparation of data sheet"),
-                        
-                        
+
+
                       )
              ),
-             
+
              #==============================About===============================#
-             
+
              tabPanel("About",
                       mainPanel(
-                        
+
                         p("by Wu and Zhang Ph.D"),
                         br(),
-                        
-                        
+
+
                       )
              )
-             
+
   )
 )
 
@@ -75,35 +75,35 @@ ui <- fluidPage(
 #################################################################################################
 
 server <- function(input, output) {
-  
+
   getData1 <- reactive({
-    
+
     file <- input$file1
     ext <- tools::file_ext(file$datapath)
-    
+
     req(file)
     validate(need(ext == "csv", "Please upload a csv file"))
-    
+
     #------------------读取数据后开始处理数据---------------#
-    
+
     data1 <- read.csv(file$datapath, header = T)
-    
+
     data1
-    
+
   })
-  
-  
+
+
   output$contents  <-  renderTable({
-    
+
     data <- getData1()
-    
+
     data
-    
-  }) 
-  
-  
-  
-  
+
+  })
+
+
+
+
 }
 
 
